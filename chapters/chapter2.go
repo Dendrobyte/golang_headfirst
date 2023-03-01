1e1cb1326475370ca1c5f31fb737a02b4b916aaf
Dendrobyte <markbacon78@gmail.com>
2023-02-28 20:05:03 -0800

    Finish chapter 2 num guesser

--- chapters/chapter2.go
+++ chapters/chapter2.go
@@ -1,9 +1,14 @@
 package chapters
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func Chapter2() {
@@ -15,5 +20,40 @@ func Chapter2() {
 		log.Fatal("Fileinfo has error")
 	}
 	fmt.Println(fileInfo.Size())
+}
 
+/* Number Guesser */
+func Ch2NumGuesser() {
+	rand.Seed(time.Now().Unix()) // Seed the randomness based in unix time
+	target := rand.Intn(100) + 1
+	fmt.Println("I've generated a number between 1 and 100... guess away!")
+	reader := bufio.NewReader(os.Stdin)
+	var x int
+	success := false
+	for x = 0; x <= 10; x++ {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal("The input errored out!")
+		}
+		input = strings.TrimSpace(input)
+		inputNum, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal("The input didn't seem acceptable... (whole numbers only)")
+		}
+		if inputNum < target {
+			fmt.Println("Your guess was too LOW!")
+		} else if inputNum > target {
+			fmt.Println("Your guess was too HIGH!")
+		} else {
+			fmt.Println("Winner winner chicken dinner!")
+			success = true
+			break
+		}
+	}
+	// Interestingly, inputNum is 0 here but is correct when we're in the above else block. Hmm.
+	if success {
+		fmt.Println("Congrats for guessing! It took you", x+1, "guesses.")
+	} else {
+		fmt.Println("Looks like you ran out of guesses, oh well! The answer was", target)
+	}
 }
--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-)
-
 func main() {
-	rand.Seed(time.Now().Unix()) // Seed the randomness based in unix time
-	target := rand.Intn(100) + 1
-	fmt.Println(target)
-	fmt.Println("I've generated a number between 1 and 100... guess away!")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal("The input errored out!")
-	}
-	input = strings.TrimSpace(input)
-	inputNum, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal("The input didn't seem acceptable... (whole numbers only)")
-	}
-	if inputNum < target {
-		fmt.Println("Your guess was too LOW!")
-	} else if inputNum > target {
-		fmt.Println("Your guess was too HIGH!")
-	} else {
-		fmt.Println("Winner winner chicken dinner!")
-	}
+
 }
