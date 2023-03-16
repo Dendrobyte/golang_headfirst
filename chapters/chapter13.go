4cc9b15561cbdfba82eda42abfb28f4d55a385a8
Dendrobyte <markbacon78@gmail.com>
2023-03-16 10:40:07 -0700

    Finish chapter 13

--- chapters/chapter13.go
+++ chapters/chapter13.go
@@ -2,21 +2,24 @@ package chapters
 
 import (
 	"fmt"
-	"time"
 )
 
 // Code Magnets
-func repeat(s string) {
+func repeat(s string, channel chan string) {
 	for i := 0; i < 25; i++ {
-		fmt.Print(s)
+		channel <- s
 	}
 }
 
 // Code Magnets
 func Chapter13() {
 
-	go repeat("x")
-	go repeat("y")
-	time.Sleep(time.Second)
+	testChan := make(chan string)
+	go repeat("x", testChan)
+	go repeat("y", testChan)
+	for i := 0; i < 50; i++ {
+		// Setting this to > 50 results in a deadlock, so go knows the channel will never update. Cool!
+		fmt.Println(<-testChan)
+	}
 
 }
